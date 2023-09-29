9b032e57aff714c94e781c8b8f7e520ae586abf3
dell <hajrm645@gmail.com>
2023-09-30 00:14:50 +0300

    new dart file

---
+++ lib/ahly.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class fhfu extends StatelessWidget {
+  const fhfu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.cyan,
+      ),
+    );
+  }
+}
